8452d11bdee22af7567f5582f4e3c192d9f667d1
giuliojiang <giuliojiang@gmail.com>
2018-05-12 14:46:09 +0100

    Import external libraries and implement image loading

--- gui/static/mainController.js
+++ gui/static/mainController.js
@@ -2,6 +2,12 @@ var mainApp = angular.module("mainApp", []);
 
 mainApp.controller("main_controller", function($scope) {
 
-    $scope.title = "THIS IS MY TITLE";
+    $scope.title = "pbrt v3 IILE";
+
+    $scope.buttonCombined = function() {
+        console.info("Clicked buttonCombined");
+        var imgPath = "/home/gj/git/pbrt-v3-scenes-extra/staircase2/TungstenRender.png";
+        loadImage("img_main", imgPath);
+    };
 
 });
--- gui/static/root.js
+++ gui/static/root.js
@@ -1,6 +1,8 @@
 staticdir = __dirname;
 
-var createPath = function(pname) {
+var url = require("url");
+
+var pathToUrl = function(pname) {
     return url.format({
         pathname: pname,
         protocol: "file:",
@@ -8,3 +10,15 @@ var createPath = function(pname) {
     });
 };
 
+var loadImage = function(targetId, imagePath) {
+    var elem = document.getElementById(targetId);
+    // Remove all children of the target element
+    while (elem.firstChild) {
+        elem.removeChild(elem.firstChild);
+    }
+    // Create new IMG node
+    var img = document.createElement("img");
+    img.src = pathToUrl(imagePath);
+    // Add the node
+    elem.appendChild(img);
+};
