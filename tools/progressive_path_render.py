0dc3658050c6c59a088dd240a9cef11b7e284e77
giuliojiang <giuliojiang@gmail.com>
2018-05-16 11:01:54 +0100

    Update progressive path renderer

--- tools/progressive_path_render.py
+++ tools/progressive_path_render.py
@@ -1,13 +1,14 @@
 import os
 import subprocess
+import time
 
 # =============================================================================
 # Constants and settings
 
 inputFiles = [
     "/home/gj/git/pbrt-v3-scenes/white-room/whiteRoomDaytimePath.pbrt",
-    "/home/gj/git/pbrt-v3-scenes-extra/bedroom/scenePath.pbrt",
-    "/home/gj/git/pbrt-v3-scenes-extra/veach-ajar/scenePath.pbrt"
+    "/home/gj/git/pbrt-v3-scenes-extra/veach-ajar/scenePath.pbrt",
+    "/home/gj/git/pbrt-v3-custom-scenes/mbed1/scenePath.pbrt"
 ]
 
 outputDir = "/home/gj/git/pbrt-v3-IISPT/tmp"
@@ -38,9 +39,18 @@ def processFileAtQuality(fpath, spp):
     sceneName = os.path.basename(fdir)
     outFileName = "{}_{}.pfm".format(sceneName, spp)
     outFilePath = os.path.join(outputDir, outFileName)
+    statFileName = "{}_{}.txt".format(sceneName, spp)
+    statFilePath = os.path.join(outputDir, statFileName)
+
+    # Skip if already processed
+    if os.path.exists(statFilePath):
+        return
 
     # Change working directory
     os.chdir(fdir)
+
+    # Start timer
+    timeStart = time.time()
 
     # Start process
     cmd = []
@@ -48,6 +58,16 @@ def processFileAtQuality(fpath, spp):
     cmd.append(fpath)
     cmd.append(outFilePath)
     runProcess(cmd)
+
+    # End timer
+    timeEnd = time.time()
+    secondsElapsed = timeEnd - timeStart
+    secondsElapsed = int(secondsElapsed)
+
+    # Record on file
+    statFile = open(statFilePath, "w")
+    statFile.write("{}\n".format(secondsElapsed))
+    statFile.close()
 
 def processFile(fpath):
     spp = 1
