300646e91cc4b8e1daa8cc5190dfbf7c83379519
giuliojiang <giuliojiang@gmail.com>
2018-05-19 15:19:40 +0100

    Make charts for path and iile whiteroom

--- tools/ssim_cmd.py
+++ tools/ssim_cmd.py
@@ -9,6 +9,7 @@ sys.path.append(mlDir)
 
 import pfm
 import scipy.stats
+import numpy
 
 argv = sys.argv[1:]
 
@@ -27,3 +28,24 @@ data = testImg.data
 data = data.flatten()
 entropy = scipy.stats.entropy(data)
 print("Entropy {}".format(entropy))
+
+# Compute SNR
+mean = numpy.mean(data)
+std = numpy.std(data)
+print("SNR {}".format(mean / std))
+
+# Shannon entropy
+
+def nozero(x):
+    if x <= 0.0:
+        return 0.000001
+    return x
+
+# data = testImg.data
+# print(data.shape)
+# a = data.transpose((2, 0, 1))
+# a = numpy.vectorize(nozero)(a)
+# a = a / a.sum()
+# print(a.shape)
+# shannon = -numpy.sum(a*numpy.log2(a))
+# print("Shannon entropy {}".format(shannon))
